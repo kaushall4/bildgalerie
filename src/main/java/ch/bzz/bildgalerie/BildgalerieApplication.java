e0f97f54f81b317e716c72a5090b16ba20c7f9ff
Kaushall Vimalarajah <v.kaushi4@gmail.com>
2022-03-10 14:30:15 +0100

    Initial commit

---
+++ src/main/java/ch/bzz/bildgalerie/BildgalerieApplication.java
@@ -0,0 +1,13 @@
+package ch.bzz.bildgalerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BildgalerieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BildgalerieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bzz/bildgalerie/BildgalerieApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bzz.bildgalerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BildgalerieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
