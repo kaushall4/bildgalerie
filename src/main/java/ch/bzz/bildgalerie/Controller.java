4ae47b52007c220486ecfc077b9fb02575da12b2
Kaushall Vimalarajah <v.kaushi4@gmail.com>
2022-03-31 15:49:20 +0200

    spring Boot

---
+++ src/main/java/ch/bzz/bildgalerie/Controller.java
@@ -0,0 +1,15 @@
+package ch.bzz.bildgalerie;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @RequestMapping("/index")
+    String index(Model model){
+        return "index.html";
+    }
+
+
+}
