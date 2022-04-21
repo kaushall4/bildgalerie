e5cd8f1fa077e14eec9cc713fff9bb1e769c15f7
Kaushall Vimalarajah <v.kaushi4@gmail.com>
2022-04-21 16:22:07 +0200

    Grid layout

--- src/main/java/ch/bzz/bildgalerie/Controller.java
+++ src/main/java/ch/bzz/bildgalerie/Controller.java
@@ -1,7 +1,7 @@
 package ch.bzz.bildgalerie;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
